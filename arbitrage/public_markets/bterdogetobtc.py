4afb15d637e6fed99cc1f7cbf0d24cc2d1715b84
alex <alex@alex-ThinkPad-W520>
2014-01-10 23:35:50 -0800

    Added Bter market

--- arbitrage/public_markets/bterdogetobtc.py
+++ arbitrage/public_markets/bterdogetobtc.py
@@ -7,32 +7,32 @@ import json
 from .market import Market
 
 #http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132
-class CoinsEDOGEtoBTC(Market):
+class BterDOGEtoBTC(Market):
     def __init__(self):
-        super(CoinsEDOGEtoBTC, self).__init__("USD")
+        super(BterDOGEtoBTC, self).__init__("USD")
         self.update_rate = 60
 
     def update_depth(self):
         #Dogecoin/BTC exchange URL
-        url = 'https://www.coins-e.com/api/v2/market/DOGE_BTC/depth/'
+        url = 'https://bter.com/api/1/depth/doge_btc'
         res = urllib.request.urlopen(url)
         depth = json.loads(res.read().decode('utf8'))
         self.depth = self.format_depth(depth)
 
     def sort_and_format(self, orders, reverse=False):
-        orders.sort(key=lambda x: float(x['r']), reverse=reverse)
+        orders.sort(key=lambda x: float(x[0]), reverse=reverse)
         r = []
         for i in orders:
-            r.append({'price': float(i['r']), 'amount': float(i['q'])})
+            r.append({'price': float(i[0]), 'amount': float(i[1])})
         return r
 
     def format_depth(self, depth):
         bids = self.sort_and_format(
-            depth['marketdepth']['bids'], True)
+            depth['bids'], True)
         asks = self.sort_and_format(
-            depth['marketdepth']['asks'], False)
+            depth['asks'], False)
         return {'asks': asks, 'bids': bids}
 
 if __name__ == "__main__":
-    market = CoinsEDOGEtoBTC()
+    market = BterDOGEtoBTC()
     print(market.get_ticker())
