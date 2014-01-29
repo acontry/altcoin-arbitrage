bbf3c3b83f35f12f6ef7b05a8b150dcd1a37c72f
Alex Contryman <acontry@gmail.com>
2014-01-28 23:58:04 -0800

    Delete Dominik folder

--- bter_api/bter_login.py
+++ bter_api/bter_login.py
@@ -6,20 +6,20 @@ from PIL import Image
 
 
 def login(email, password):
-	s = requests.Session()
-	r = s.get('https://bter.com/captcha')
-	i = Image.open(io.BytesIO(r.content))
-	i.save('temp.png')
+    s = requests.Session()
+    r = s.get('https://bter.com/captcha')
+    i = Image.open(io.BytesIO(r.content))
+    i.save('temp.png')
 
-	captcha = parse_captcha.parse_captcha('temp.png')
-	print('Captcha: ' + captcha)
+    captcha = parse_captcha.parse_captcha('temp.png')
+    print('Captcha: ' + captcha)
 
-	params = {'email':'email', 'password': 'pass', 'captcha': captcha, 'submit': 'Submit'}
+    params = {'email':'email', 'password': 'pass', 'captcha': captcha, 'submit': 'Submit'}
 
-	r = s.post('https://bter.com/login', params = params)
-	#print(r.content)
+    r = s.post('https://bter.com/login', params = params)
+    #print(r.content)
 
 if __name__ == '__main__':
-	email = sys.argv[1]
-	password = sys.argv[2]
-	login(email, password)
+    email = sys.argv[1]
+    password = sys.argv[2]
+    login(email, password)
