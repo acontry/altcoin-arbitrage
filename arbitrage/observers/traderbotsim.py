aa39c76e77af9a393c69d0b0f8a7078c2999e1d1
Alex Contryman <acontry@gmail.com>
2014-01-29 01:16:53 -0800

    Delete unused files

--- arbitrage/observers/traderbotsim.py
+++ arbitrage/observers/traderbotsim.py
@@ -1,8 +1,6 @@
 import logging
 import config
 from .observer import Observer
-from private_markets import mtgox
-from private_markets import bitcoincentral
 from .traderbot import TraderBot
 import json
 
