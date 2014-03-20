5a48eea144374a9b6622d05cd00f361ab52a371f
Alex Contryman <acontry@gmail.com>
2014-03-19 18:20:54 -0700

    Trade confirmation implementation and update nonce generation

--- arbitrage/private_markets/coinse.py
+++ arbitrage/private_markets/coinse.py
@@ -1,4 +1,3 @@
-# Copyright (C) 2013, Maxime Biais <maxime@biais.org>
 
 from .market import Market, TradeException
 import time
@@ -7,6 +6,7 @@ import urllib.parse
 import requests
 import hashlib
 import config
+import database
 
 
 class PrivateCoinsE(Market):
@@ -23,7 +23,7 @@ class PrivateCoinsE(Market):
         url = self.api_url + method_url
         # generate POST data string
         #req["nonce"] = int(10 * time.time()) - 13926910527
-        req["nonce"] = int(time.time())
+        req["nonce"] = int(time.time() * 10)
         post_data = urllib.parse.urlencode(req)
         # sign it
         sign = hmac.new(self.secret.encode("ascii"), post_data.encode("ascii"), hashlib.sha512).hexdigest()
@@ -37,29 +37,53 @@ class PrivateCoinsE(Market):
         try:
             value = res.json()
         except Exception as e:
-            raise Exception("Unable to decode response from %s - %s" %
-                            (self.name, e))
+            raise Exception("Unable to decode response from %s - %s" % (self.name, e))
         return value
 
-    def _place_order(self, amount, price, type):
+    def _place_order(self, amount, price, order_type):
         """Place a buy/sell order. type should be buy or sell."""
         url = "market/" + self.p_coin + "_" + self.s_coin + "/"
-        params = {"method": "neworder", "order_type": type, "rate": price, "quantity": amount}
+        params = {"method": "neworder", "order_type": order_type, "rate": price, "quantity": amount}
         return self.query(url, params)
 
     def _buy(self, amount, price):
         """Create a buy limit order"""
         response = self._place_order(amount, price, "buy")
-        if response["status"] != True:
+        if not response["status"]:
             raise TradeException(response["error"])
         return response["order"]["id"]
 
     def _sell(self, amount, price):
         """Create a sell limit order"""
         response = self._place_order(amount, price, "sell")
-        if response["status"] != True:
+        if not response["status"]:
             raise TradeException(response["error"])
         return response["order"]["id"]
+
+    def get_open_orders(self):
+        url = "market/" + self.p_coin + "_" + self.s_coin + "/"
+        response = self.query(url, {'method': 'listorders', 'filter': 'active'})
+        if not response['status']:
+            raise TradeException(response['error'])
+        return response['orders']
+
+    def update_order_status(self):
+        if not self.open_orders:
+            return
+        response = self.get_open_orders()
+        remaining_open_orders = []
+        completed_order_ids = []
+        for open_order in self.open_orders:
+            found_order = [found_order for found_order in response if
+                           found_order['id'] == open_order['order_id']]
+            if not found_order:
+                completed_order_ids.append(open_order['order_id'])
+            else:
+                remaining_open_orders.append(open_order)
+
+        if completed_order_ids:
+            self.open_orders = remaining_open_orders
+            database.order_completed(self.name, completed_order_ids)
 
     def get_balances(self):
         """Get balance of primary coin and secondary coin"""
